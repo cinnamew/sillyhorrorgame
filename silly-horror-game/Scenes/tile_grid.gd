db3d94a20ca6700edb4796bd125f12cab375e55a
Sohail Sidique <ssidique8@gmail.com>
2025-07-02 21:30:45 -0700

    Chunk Generator

--- silly-horror-game/Scenes/tile_grid.gd
+++ silly-horror-game/Scenes/tile_grid.gd
@@ -7,31 +7,57 @@ const NUM_LAYERS = 3
 const BLOCK_HEIGHT = 16
 
 var layers = []
-var grid = [
-		   [[Tiles.TILE_DIRT, Tiles.TILE_DIRT, Tiles.TILE_DIRT], 
-			[Tiles.TILE_DIRT, Tiles.TILE_DIRT, Tiles.TILE_DIRT], 
-			[Tiles.TILE_DIRT, Tiles.TILE_DIRT, Tiles.TILE_DIRT],
-			[Tiles.TILE_DIRT, Tiles.TILE_DIRT, Tiles.TILE_DIRT]],
-			
-		   [[Tiles.TILE_GRASS, Tiles.TILE_GRASS, Tiles.TILE_GRASS], 
-			[Tiles.TILE_GRASS, Tiles.TILE_GRASS, Tiles.TILE_GRASS], 
-			[Tiles.TILE_GRASS, Tiles.TILE_GRASS, Tiles.TILE_GRASS]]
-			]
+var grid = []
+
+var rng = RandomNumberGenerator.new()
 
 enum Tiles {
 	TILE_DIRT,
 	TILE_GRASS,
 	TILE_ICE,
+	TILE_STONE,
 }
 
 const tile_data = {
 	Tiles.TILE_DIRT: Vector2i(3,0),
-	Tiles.TILE_GRASS: Vector2i(0, 2)
+	Tiles.TILE_GRASS: Vector2i(0, 2),
+	Tiles.TILE_STONE: Vector2i(8, 5),
 }
 
 func _ready():
 	setup_layers()
+	generate_map()
 	render_grid()
+	
+func generate_map():
+	print("Generating Map")
+	# Bedrock phase
+	var layer = []
+	for x in range(CHUNK_SIZE):
+		var row = []
+		for y in range(CHUNK_SIZE):
+			row.append(Tiles.TILE_STONE)
+		layer.append(row)
+	grid.append(layer)
+	
+	# Dirt Layer
+	layer = []
+	for x in range(CHUNK_SIZE):
+		var row = []
+		for y in range(CHUNK_SIZE):
+			row.append(Tiles.TILE_DIRT)
+		layer.append(row)
+	grid.append(layer)
+	
+	# Grass Layer
+	layer = []
+	for x in range(CHUNK_SIZE):
+		var row = []
+		for y in range(CHUNK_SIZE):
+			row.append(Tiles.TILE_GRASS)
+		layer.append(row)
+	grid.append(layer)
+	
 	
 func setup_layers():
 	layers.append($layer1)
